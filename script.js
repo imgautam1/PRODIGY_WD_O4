3b7961ac146eb5ada8db468ac8e1a9a184e12985
Gautam Makwana <156568282+imgautam1@users.noreply.github.com>
2024-12-24 10:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+ /* All right are reserved by Gautam Makwana © Copyright */
